d0f1c101b05c2c5184897d333cf8b3578a8913e1
denyssergeev <85879343+DenysSergeev@users.noreply.github.com>
2022-06-22 15:04:07 +0300

    new changes

--- src/Components/MuiTable.js
+++ src/Components/MuiTable.js
@@ -7,189 +7,80 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-//import { MOCK_DATA } from '../utils/mocks/tableData';
+import { MOCK_DATA } from "../utils/mocks/tableData";
 
+const MuiTable = React.memo(() => {
+  const years = React.useMemo(getYears, []);
 
-const MuiTable = () => {
+  function getOnlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+  function getYears() {
+    const values = Object.values(MOCK_DATA).map((el) => Object.values(el));
+    let years = [];
+
+    values.forEach((el) => {
+      const elYears = el.map((key) => Object.keys(key));
+
+      elYears.forEach((year) => {
+        years = [...years, ...year];
+      });
+    });
+    return years.filter(getOnlyUnique);
+  }
+
+  const renderBodyRow = (region) => {
+    const regionDataByYears = Object.values(MOCK_DATA[region]) [0];
 
     return (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 1000 }} aria-label="simple table">
-            <TableHead>
-                <TableCell>Regions</TableCell>
-                <TableCell align="center">2017
-                    <TableCell align="center">XX</TableCell>
-                    <TableCell align="center">YY</TableCell>
-                    <TableCell align="center">ZZ</TableCell>
-                </TableCell>
-                <TableCell align="center">2018
-                    <TableCell align="center">XX</TableCell>
-                    <TableCell align="center">YY</TableCell>
-                    <TableCell align="center">ZZ</TableCell>
-                </TableCell>
-                <TableCell align="center">2019
-                    <TableCell align="center">XX</TableCell>
-                    <TableCell align="center">YY</TableCell>
-                    <TableCell align="center">ZZ</TableCell>
-                </TableCell>
-            </TableHead>
-            <TableBody>
-              {MOCK_DATA.map((row) => (
-                <TableRow
-                  key={row.Kyivska}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                    <TableCell>Kyivska</TableCell>
-                    <TableCell align="left">{row.Kyivska.G[2017].XX.value}</TableCell>
-                    <TableCell align="left">{row.Kyivska.G[2017].YY.value}</TableCell>
-                    <TableCell align="left">{row.Kyivska.G[2017].ZZ.value}</TableCell>
-                </TableRow>
-              
-              ))}
-              {MOCK_DATA.map((row) => (
-                <TableRow
-                  key={row.Kyivska}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                    <TableCell>Odeska</TableCell>
-                    <TableCell align="right">{row.Odeska.G[2017].XX.value}</TableCell>
-                    <TableCell align="right">{row.Odeska.G[2017].YY.value}</TableCell>
-                    <TableCell align="right">{row.Odeska.G[2017].ZZ.value}</TableCell>
-                </TableRow>
-              
-              ))}
-              {MOCK_DATA.map((row) => (
-                <TableRow
-                  key={row.Kyivska}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                    <TableCell>Lvivska</TableCell>
-                    <TableCell align="right">{row.Lvivska.G[2017].XX.value}</TableCell>
-                    <TableCell align="right">{row.Lvivska.G[2017].YY.value}</TableCell>
-                    <TableCell align="right">{row.Lvivska.G[2017].ZZ.value}</TableCell>
-                </TableRow>
-              
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer> 
+      <TableRow key={region}>
+        <TableCell>{region}</TableCell>
+
+        {years.map((year) => {
+          const regionDataByYear = regionDataByYears[year];
+
+          if (!regionDataByYear) {
+            return <TableCell></TableCell>;
+          }
+          return <TableCell></TableCell>;
+        })}
+      </TableRow>
     );
-}
+  };
+  const renderYears = React.useCallback(() => {
+    return years.map((year) => {
+      return (
+        <React.Fragment key={year}>
+          <TableCell align="left" />
+          <TableCell align="left">{year}</TableCell>
+          <TableCell align="left" />
+        </React.Fragment>
+      );
+    });
+  }, [years]);
 
-const MOCK_DATA = [
-    {
-        Kyivska: {
-          G: {
-            2017: {
-              XX: {
-                value: 150000,
-                dateRelease: "2017-12-31",
-              },
-              YY: {
-                value: 100000,
-                dateRelease: "2017-12-31",
-              },
-              ZZ: {
-                value: 77,
-                dateRelease: "2017-12-31",
-              },
-            },
-            2018: {
-              XX: {
-                value: 160000,
-                dateRelease: "2018-12-31",
-              },
-              YY: {
-                value: 110000,
-                dateRelease: "2018-12-31",
-              },
-              ZZ: {
-                value: 72,
-                dateRelease: "2018-12-31",
-              },
-            },
-            2019: {
-              XX: {
-                value: 130000,
-                dateRelease: "2019-12-31",
-              },
-              YY: {
-                value: 85000,
-                dateRelease: "2019-12-31",
-              },
-              ZZ: {
-                value: 72,
-                dateRelease: "2019-12-31",
-              },
-            },
-          },
-        },
-        Odeska: {
-          G: {
-            2017: {
-              XX: {
-                value: 10000,
-                dateRelease: "2017-12-31",
-              },
-              YY: {
-                value: 5000,
-                dateRelease: "2017-12-31",
-              },
-              ZZ: {
-                value: 45,
-                dateRelease: "2017-12-31",
-              },
-            },
-            2019: {
-              XX: {
-                value: 15000,
-                dateRelease: "2019-12-01",
-              },
-              YY: {
-                value: 0,
-                dateRelease: "2022-02-18",
-              },
-              ZZ: {
-                value: 0,
-                dateRelease: "2022-02-18",
-              },
-            },
-          },
-        },
-        Lvivska: {
-          G: {
-            2017: {
-              XX: {
-                value: 640000,
-                dateRelease: "2017-12-31",
-              },
-              YY: {
-                value: 510000,
-                dateRelease: "2017-08-01",
-              },
-              ZZ: {
-                value: 67,
-                dateRelease: "2017-08-01",
-              },
-            },
-            2018: {
-              XX: {
-                value: 740000,
-                dateRelease: "2018-12-31",
-              },
-              YY: {
-                value: 530000,
-                dateRelease: "2018-08-01",
-              },
-              ZZ: {
-                value: 61,
-                dateRelease: "2018-08-01",
-              },
-            },
-          },
-        },
-      }
-  ]
+  const renderTableBody = React.useCallback(() => {
+    const content = Object.keys(MOCK_DATA);
 
+    return content.map(renderBodyRow);
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 1000 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Regions</TableCell>
+            {renderYears()}
+          </TableRow>
+        </TableHead>
+
+        <TableBody>{renderTableBody()}</TableBody>
+      </Table>
+    </TableContainer>
+  );
+
+});
 
 export default MuiTable;
